702c91d92ffa0c4564255b44580809750b634e27
Andrey Matochkin <lgaragel@yandex.ru>
2025-04-03 09:45:41 +0300

    1 adjustment

--- src/pages/main_page.py
+++ src/pages/main_page.py
@@ -3,7 +3,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from src.locators.main_page_locators import MainPageLocators
 from src.pages.base_page import BasePage
 from selenium.webdriver.common.by import By
-from allure import step
+import allure
 
 
 class MainPage(BasePage):
@@ -11,34 +11,34 @@ class MainPage(BasePage):
     def __init__(self, driver):
         super().__init__(driver)
 
+    @allure.step(f'Open page')
     def open_page(self, url):
-        with step(f'Open page {url}'):
-            self.navigate(url)
+        self.navigate(url)
 
+    @allure.step(f'Click on question')
     def click_on_questions(self, locator):
-        with step(f'Click on question'):
-            self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
-            self.wait_for_element_clickable(locator).click()
+        self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
+        self.wait_for_element_clickable(locator).click()
 
+    @allure.step(f'Get answer')
     def get_answer(self, locator):
-        with step(f'Get answer'):
-            self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
-            answer = self.select_element(locator)
-            return answer.is_displayed()
+        self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
+        answer = self.select_element(locator)
+        return answer.is_displayed()
 
+    @allure.step(f'Click order button')
     def order_button_click(self, locator):
-        with step(f'Click order button'):
-            self.click_element(MainPageLocators.COOKIE_BUTTON)
-            self.wait_for_element_visibility(locator).click()
+        self.click_element(MainPageLocators.COOKIE_BUTTON)
+        self.wait_for_element_visibility(locator).click()
 
+    @allure.step(f'Click logo scooter')
     def logo_button_click(self):
-        with step(f'Click logo scooter'):
-            self.click_element(MainPageLocators.LOGO_BUTTON)
+        self.click_element(MainPageLocators.LOGO_BUTTON)
 
+    @allure.step(f'Click logo yandex')
     def logo_yandex_click(self):
-        with step(f'Click logo yandex'):
-            self.click_element(MainPageLocators.LOGO_YANDEX_BUTTON)
+        self.click_element(MainPageLocators.LOGO_YANDEX_BUTTON)
 
+    @allure.step(f'Current url')
     def get_current_url(self):
-        with step(f'Current url'):
-            return self.driver.current_url
+        return self.driver.current_url
